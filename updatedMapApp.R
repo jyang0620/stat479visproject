72fa9e765510566cfc75c097efd3e36d5fd368ca
dfyoung2 <78430622+dfyoung2@users.noreply.github.com>
2022-05-03 00:34:29 -0500

    Variables

--- updatedMapApp.R
+++ updatedMapApp.R
@@ -13,55 +13,55 @@ data$state = data$State
 counts = as.data.frame(table(data$State))
 data = inner_join(data, counts, c("state" = "Var1"))
 col = as.data.frame( data %>%
-                       group_by(state) %>% 
-                       summarise(
-                         Jobs = Freq,
-                         `Average Salary` = round(mean(`Avg Salary(K)`),3)*1000,
-                         `Average Rating` = round(mean(Rating),2)
-                         
-                       )) %>% distinct()
-# Define UI for application that draws a histogram
+                         group_by(state) %>% 
+                         summarise(
+                             Jobs = Freq,
+                             Average_salary = round(mean(`Avg Salary(K)`),3)*1000,
+                             Average_rating = round(mean(Rating),2)
+                             
+                         )) %>% distinct()
+
 
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Breaking Values Down by State"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("vars",
-                  "Select a metric",
-                  colnames(col)[2:4]
-      ),
-      h3("Variable Definitions"),
-      h5("Jobs: Number of jobs listed for hiring"),
-      h5("avg_salary: Average salary for those listed jobs"),
-      h5("avg_rating: Average Company rating for a state"),
-      h4("\n\nNote: States that are grey are missing data.")
-    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotlyOutput("map"),
-      
-      
+    # Application title
+    titlePanel("Breaking Values Down by State"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("vars",
+                        "Select a metric",
+                        colnames(col)[2:4]
+            ),
+            h3("Variable Definitions"),
+            h5("Jobs: Number of jobs listed for hiring"),
+            h5("Average_salary: Average salary for those listed jobs"),
+            h5("Average_rating: Average Company rating for a state"),
+            h4("\n\nNote: States that are grey are missing data.")
+        ),
+        
+        
+        mainPanel(
+            plotlyOutput("map"),
+            
+            
+        )
     )
-  )
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
-  output$map <- renderPlotly({
-    g = plot_usmap(data = col,values = input$vars, color = "dark green") +
-      scale_fill_continuous(
-        low = "white", high = "dark green", name = input$vars,
-        label = scales::comma
-      )+
-      theme(legend.position = "right")
-    ggplotly(g) %>%
-      style(hoveron = "fill")
-  })
+    output$map <- renderPlotly({
+        g = plot_usmap(data = col,values = input$vars, color = "dark green") +
+            scale_fill_continuous(
+                low = "white", high = "dark green", name = input$vars,
+                label = scales::comma
+            )+
+            theme(legend.position = "right")
+        ggplotly(g) %>%
+            style(hoveron = "fill")
+    })
 }
 
 # Run the application 
