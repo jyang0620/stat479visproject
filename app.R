a5eb4f23fdb3ab7579844aa6365e4b5551f3437f
ellerywuyn <47910316+ellerywuyn@users.noreply.github.com>
2022-05-04 01:39:53 -0500

    Plot2

--- app.R
+++ app.R
@@ -18,18 +18,32 @@ industry <- pull(ds_salfix, Industry) %>%
     
 industry <- sort(industry)
 
+small_samples <- ds_salfix %>% 
+    group_by(Industry) %>% 
+    summarize(n = n()) %>% 
+    arrange(n) %>% 
+    filter(n<= 5) %>% 
+    pull(Industry)
+
+
+ds_salfix <- ds_salfix %>% 
+    mutate(Industry = replace(Industry, Industry %in% small_samples, "Other"))
+
+
 # Boxplot
 box <- function(df){    
     ggplot(data = df %>% filter(selected)) +
         geom_boxplot(aes(x = Avg.Salary.K, 
-                         y = fct_reorder(Industry, Avg.Salary.K, median),
-                         fill = Industry)) + #suggestion: reorder box plots
+                         y = fct_reorder(Industry, Avg.Salary.K, median)
+                         # fill = Industry
+                         )) + 
+        facet_wrap(~Sector) +#suggestion: reorder box plots
         theme(legend.position="none",
               axis.text.y = element_text(size = 14),
               axis.text.x = element_text(size = 14),
               axis.title = element_text(size = 16),
               plot.title = element_text(size = 18)) +
-        scale_fill_brewer(palette = "Spectral")+
+        # scale_fill_brewer(palette = "Spectral")+
         labs(title= "Average Salaries vs Industry", 
              x= "Average Salary in Thousands",
              y= "Industry")
@@ -37,7 +51,7 @@ box <- function(df){
 ui <- fluidPage(
     titlePanel(h1("Average Salaries for Data Science Jobs", align = "center")),
     sidebarLayout(
-        sidebarPanel(checkboxGroupInput("industry", "Industry", industry, industry[1:12])),
+        sidebarPanel(selectInput("industry", "Industry", industry, multiple = TRUE)),
         mainPanel(plotOutput("boxplot"))
     )
 )
