2f118b2c9a3f50e4f492e3642ae84d96580f679e
jyang0620 <jxyang@wisc.edu>
2022-05-07 16:38:45 -0500

    Added Introduction, Body Text and Conclusion to the Web App

--- WebApp.R
+++ WebApp.R
@@ -96,21 +96,32 @@ ui <- fluidPage(
     theme= shinytheme("united"),
     
     mainPanel(
-      "Data Science Salaries", 
       tabsetPanel(
         tabPanel(
           "Introduction", 
-          p("p creates a paragraph of text."),
-          p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+          strong("Introduction", style = "font-family: 'times'"), br(),
+          p("According to the US Department of Labor Statistics, as of May 2020, the current average salary of a data scientist is $100,560, indicating a monetarily successful profession. In order to understand, a set of questions as potential points of interest were developed, pertaining to which states have the highest paying jobs, what is the most optimal job in terms of net earnings and which industries and/or sectors have the highest paying jobs. The aim of this project is to visualize the state of this profession, providing aid in the decision making process for students thinking about becoming data scientists in the future and the current data scientists searching for the optimal position.", style = "font-family: 'times'; font-si16pt"), br(),
+          strong("Data", style = "font-family: 'times'"), br(),
+          p("The data we used was scrapped from glassdoor, a review aggregation website of companies. Glassdoor allows users to anonymously rate companies, submit salaries and apply for jobs, all important for this project. The initial kaggle dataset is pre-cleaned and filtered, already containing information about salary, job descriptions and company ratings. After filtering out unwanted variables, removing those with missing data and mutating our columns, our newly processed data contains the Company Name, the city and state the job is located in, Rating, Industry, Sector, Job Title and two salary related columns: one with Average reported salary ranges and one with the calculated Average Salary based on the Salary Estimate.", style = "font-family: 'times'; font-si16pt"), br(),
+          p("Additionally, we appended a column containing the cost of living index, obtaining the numbers from a secondary calculated dataset from AdvisorSmith, a small business analytics resource. The cost of living index takes the cost of living of a place and normalizes the number across the entire nation. Similar to how OPS+ works in baseball, AdvisorSmith adjusts the number on a scale where 100 represents the national average, meaning a cost of living index of 125 means that a place is 25% more expensive than an average place. As a result, this creates a standardized statistic that can be both easily grasped and visualized with.", style = "font-family: 'times'; font-si16pt"), br(),
+          p("While appending to our kaggle dataframe, a major hurdle occurs as AdvisorSmith only accounts for the major cities, not the suburbs found within their respective metropolitan areas. This is most prominently seen for jobs located in the San Francisco Bay Area, as we had to discard jobs located in cities such as Palo Alto, Foster City and Santa Clara. To solve this problem, we manually added a column in both our salary and cost of living datasets of the metropolitan area of each unique city using excel. As a result, it allows us to join the two based on the metropolitan area column, filling in the missing cost of living values of the suburbs.", style = "font-family: 'times'; font-si16pt"), br(),
+          p("Once we obtained our generalized cleaned data set, each visualization did their own set of transformations. The first plot required a left join to sort all states by region using the rnaturalearth package. The second required grouping the data by industry and calculating the salary values based on it. For the final visualization, the process was similar through the grouping the values by state. These resulting cleaned data sets were then plotted in the subsequent visualizations.", style = "font-family: 'times'; font-si16pt"), br(),
+          p("To access our visualizations, click the headers at the top of the page!", style = "font-family: 'times'; font-si16pt")
         ),
         tabPanel(
           "First Visualization", 
           plotlyOutput("state_scatter"),
-          text()
+          text(), br(),
+          strong("Description", style = "font-family: 'times'"), br(),
+          p("The first visualization is a scatter plot that seeks to find the most optimal job in terms of salary and cost of living. It graphs the average reported salary for that specific job in that company on the x-axis and the cost of living index on the y-axis, creating a dynamic to where the ideal values are located towards the bottom right corner. Each point is colored by the region the job is based in, with the states of California, Massachusetts and New York defined as their own region. To add more interactivity for exploration and obtaining specific information about a point, a tooltip was added that brings up information regarding the specific point upon hover. Lastly, an appendix was added below the graph, defining the values of the axes and the specific states in each defined geographic region.", style = "font-family: 'times'; font-si16pt"), br(),
+          p("From the graph, a key observation is that there is no correlation between the two variables, with the points plotted seemingly randomly. This is slightly counterintuitive, as one would expect the salary to rise in conjunction with living costs. Additionally, all the jobs based in the San Francisco Bay Area are plotted along a straight line, well separated from the rest of the points. Lastly, the most bottomright and ideal point represents a job for a Director of Data Science in Chicago, indicating that the most ideal position is located in the Midwest.	", style = "font-family: 'times'; font-si16pt"), br(),
         ),
         tabPanel(
           "Second Visualization", 
           titlePanel(h1("Average Salaries for Data Science Jobs", align = "center")),
+          strong("Description", style = "font-family: 'times'"), br(),
+          p("In this visualization, we seek to answer the question of what industries provide the highest-paying data science related jobs. In this interactive visualization, users can select industries of their interest to view and compare their relative box plots in terms of average annual salary in thousands. Boxplots are ordered by descending median values.", style = "font-family: 'times'; font-si16pt"), br(),
+          p("Some findings from this visualization includes that data science jobs in financial analytics, telecommunication services, and brokerage are among the highest paying ones; while similar jobs in food and beverage manufacturing, architectural and engineering services, and social assistance industries are among the lowest paying ones.", style = "font-family: 'times'; font-si16pt"),
           sidebarLayout(
             sidebarPanel(selectInput("industry", 
                                      "Industry", 
@@ -142,12 +153,18 @@ ui <- fluidPage(
             
             mainPanel(
               plotlyOutput("map"),
-            )
+              strong("Description", style = "font-family: 'times'"), br(),
+              p("This third visualization is a map that uses a continuous scale to display the average job rating, job availability and average salary. The color of the state is directly correlated to each of the metrics. The states with the higher value become a darker shade of green and have a lighter, paler shade of green for a lower value. When hovering the cursor of each state, the selected metric for that will appear. There is a legend detailing the exact scale used in the visual. An important observation from the graph is the fact that particular states are missing color.  We see this as a possible indicator that there aren’t any jobs of interest in those missing states or that a person is interested in data analytics or science.", style = "font-family: 'times'; font-si16pt"), br(),
+            ),
+            
           )
         ),
         tabPanel(
           "Conclusion", 
-          p("Hello World!")
+          strong("Conclusion", style = "font-family: 'times'"), br(),
+          p("After creating the visuals and reviewing them, our visualizations indicate that working in the Midwestern region of the country in financial analytics, telecommunication services or brokerage is the best way to optimize personal finances when only considering cost of living, average salary, and industry of employment, even if the availability doesn't compare to other regions. On the other hand, we can conclude that while a large majority of jobs are located in California, living in the state and working in social assistance or beverage manufacturing would leave someone with a minimal amount of money in their bank account. However, it is important to remember that this conclusion is only useful for the states that were provided in the data set and listed on glassdoor.", style = "font-family: 'times'; font-si16pt"), br(),
+          strong("References", style = "font-family: 'times'"), br(),
+          img(src = "https://uwmadison.box.com/shared/static/tx32osgpvzn2ewuak6wcdu1y5jac4g64.png", width = 600, height = 300), br(),
         )
       )
     )
